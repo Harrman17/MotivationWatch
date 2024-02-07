59fe8a6c9cccfa3e8b58c1ed16785f441b282d8f
Harrman17 <88928675+Harrman17@users.noreply.github.com>
2024-02-07 22:10:21 +0000

    test

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,9 +1,31 @@
-import React , { useState } from 'react'
+import React , { useState, useEffect } from 'react'
 
 
 export default function Stopwatch() {
+
+  const [time,setTime] = useState(0)
+  const [isRunning,setIsRunning] = useState(false)
+
+  let interval
+  useEffect(() => {
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime + 1)
+      }, 1000)
+
+      return () => clearInterval(interval)
+  }
+}, [isRunning])
+
+  const StartStop = () => {
+    setIsRunning(!isRunning)
+  }
+
+
   return (
-    <div>
+    <div className='flex flex-col justify-center items-center text-white'>
+      <h1 className='font-inter text-3xl'>{time}</h1>
+      <button onClick={StartStop}>test</button>
     </div>
   )
 }
