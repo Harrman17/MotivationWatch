f358034dd7b8518f514182d8d321fe54c1aa9841
Harrman17 <88928675+Harrman17@users.noreply.github.com>
2024-02-11 17:10:07 +0000

    watch functionality

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -3,29 +3,47 @@ import React , { useState, useEffect } from 'react'
 
 export default function Stopwatch() {
 
-  const [time,setTime] = useState(0)
+  const [seconds,setSeconds] = useState(0)
   const [isRunning,setIsRunning] = useState(false)
+  const [minutes,setMinutes] = useState(0)
 
   let interval
   useEffect(() => {
     if (isRunning) {
       interval = setInterval(() => {
-        setTime(prevTime => prevTime + 1)
+        setSeconds(prevSeconds => prevSeconds + 1)
       }, 1000)
 
       return () => clearInterval(interval)
   }
 }, [isRunning])
 
+  useEffect(() => {
+    if (seconds === 20) {
+      setSeconds(0)
+      setMinutes(prevMinute => prevMinute + 1)
+    }
+  }, [seconds])
+
   const StartStop = () => {
     setIsRunning(!isRunning)
   }
 
+  const reset = () => {
+    setIsRunning(false)
+    setMinutes(0)
+    setSeconds(0)
+  }
 
   return (
     <div className='flex flex-col justify-center items-center text-white'>
-      <h1 className='font-inter text-3xl'>{time}</h1>
-      <button onClick={StartStop}>test</button>
+      <div className='bg-test flex flex-col'>
+        <h1 className='font-inter text-3xl'>{minutes < 10 ? '0' : ''}{minutes}:{seconds < 10 ? '0' : ''}{seconds}</h1>
+        <div>
+          <button className='font-inter' onClick={reset}>RESET</button>
+          <button onClick={StartStop} className='font-inter'>{isRunning ? 'STOP' : 'START'}</button>
+        </div>
+      </div>
     </div>
   )
 }
