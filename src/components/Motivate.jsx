afb8d4babf6516763edfda613bb3263a4b2be04b
Harrman17 <88928675+Harrman17@users.noreply.github.com>
2024-06-23 22:36:09 +0100

    adding videos

--- src/components/Motivate.jsx
+++ src/components/Motivate.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFire } from '@fortawesome/free-solid-svg-icons'
-
+import VideoLinks from '/src/VideoLinks.json'
 
 export default function Motivate() {
+
+  const [displayVideo, setdisplayVideo] = useState(false)
+  const [displayLink, setDisplayLink] = useState()
+
+  function randomVideo() {
+    setdisplayVideo(true)
+    setDisplayLink(VideoLinks[Math.floor(Math.random() * VideoLinks.length)])
+    console.log(displayLink)
+  }
+ 
+
   return (
     <div className='fixed bottom-2 right-8 p-8'>
-      <FontAwesomeIcon className='text-accent h-9' icon={faFire} />
+      <FontAwesomeIcon className='text-accent h-9' icon={faFire} onClick={randomVideo}/>
     </div>
   )
 }
---
+++ src/components/VideoDisplay.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function VideoDisplay() {
+
+
+  return (
+    <div>
+      <video autoPlay muted loop playsInline src={"https://youtu.be/0Hfb4qtpqlg"} />
+    </div>
+  )
+}
