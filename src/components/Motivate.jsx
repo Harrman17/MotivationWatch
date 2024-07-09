911c6671ea4432a85f8d59ad0ab248ee2e60eac8
Harrman17 <88928675+Harrman17@users.noreply.github.com>
2024-07-09 18:18:18 +0100

    added looping with hashmap to videos

--- src/components/Motivate.jsx
+++ src/components/Motivate.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFire } from '@fortawesome/free-solid-svg-icons';
 
-export default function Motivate({ randomVideo }) {
+export default function Motivate({ hashRandomVideo }) {
   return (
     <div className='fixed bottom-2 right-8 p-8'>
-      <FontAwesomeIcon className='text-accent h-9' icon={faFire} onClick={randomVideo} />
+      <FontAwesomeIcon className='text-accent h-9' icon={faFire} onClick={hashRandomVideo} />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   "./src/**/*.{js,ts,jsx,tsx}",
 ],
   theme: {
+    screens: {
+      'lg': '1920px'
+    },
     colors: {
       'primary': '#252525',
       'white': '#F2F2F2',
