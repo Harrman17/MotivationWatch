3a01f49bd543fefc43df329c4916074b2f75a3dc
Harrman17 <88928675+Harrman17@users.noreply.github.com>
2024-07-09 18:18:18 +0100

    added looping with hashmap to videos

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,45 @@ import Stopwatch from './components/Stopwatch'
 import Header from './components/Header'
 import VideoDisplay from './components/VideoDisplay'
 import Motivate from './components/Motivate'
-import VideoLinks from '/src/VideoLinks.json'
+import VideoLinksFile from '/src/VideoLinks.json'
 
 export default function App() {    
     
       const [displayVideo, setDisplayVideo] = useState(false)
       const [videoLink, setVideoLink] = useState("")
-    
-      function randomVideo() {
-        setDisplayVideo(true)
-        setVideoLink(VideoLinks[Math.floor(Math.random() * VideoLinks.length)])
-    
-        console.log(videoLink)
-        console.log(displayVideo)
+      const [map, setMap] = useState(new Map())
+
+      function getRandomIndex() {
+        return Math.floor(Math.random() * VideoLinksFile.length)
       }
+
+      function hashRandomVideo() {
+        let randomIndex = getRandomIndex()
+
+        while (map.has(randomIndex)) { // if the map already has the index, call a new one
+          randomIndex = getRandomIndex()
+        } 
+    
+
+        setMap(prevMap => { // creates a new map and sets it with the new index
+          const newMap = new Map(prevMap)
+          newMap.set(randomIndex)
+          return newMap
+        })
+
+        setVideoLink(VideoLinksFile[randomIndex])
+        setDisplayVideo(true)
+
+        }
+
 
 
     return (
         <div>
             <Header />
-             <Stopwatch />
-            <VideoDisplay displayVideo={displayVideo} videoLink={videoLink}/>
-            <Motivate randomVideo={randomVideo}/>
+            <Stopwatch />
+            <VideoDisplay displayVideo={displayVideo} videoLink={videoLink} onEnded={hashRandomVideo}/>
+            <Motivate hashRandomVideo={hashRandomVideo}/>
         </div>
     )
 }
--- src/components/VideoDisplay.jsx
+++ src/components/VideoDisplay.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 
-export default function VideoDisplay({ videoLink, displayVideo}) {
+export default function VideoDisplay({ videoLink, displayVideo, onEnded}) {
 
 
   return (
     <div className='video-container flex justify-center items-center'>
-      {displayVideo && <video autoPlay playsInline src={videoLink} className='h-5/6'/>}
+      {displayVideo && <video autoPlay playsInline src={videoLink} className='h-5/6' onEnded={onEnded}/>}
     </div>
   )
 }
