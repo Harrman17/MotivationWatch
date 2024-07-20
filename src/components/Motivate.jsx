0add32799057fd5ad273fe65f74ed13623b9fe66
Harrman17 <88928675+Harrman17@users.noreply.github.com>
2024-07-20 17:53:54 +0100

    favicon + skip video added

--- src/components/Motivate.jsx
+++ src/components/Motivate.jsx
@@ -3,11 +3,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFire, faForwardStep } from '@fortawesome/free-solid-svg-icons';
 
 
-export default function Motivate({ motivatetest, displayVideo }) {
+export default function Motivate({ videoPlayer, displayVideo, hashRandomVideo }) {
   return (
     <div className='flex fixed bottom-2 right-8 p-8 '>
-      { displayVideo && <FontAwesomeIcon className='text-accent h-9' icon={faForwardStep} /> }
-      <FontAwesomeIcon className='text-accent h-9 ml-10' icon={faFire} onClick={motivatetest} />
+      { displayVideo && <FontAwesomeIcon className='text-accent h-9' icon={faForwardStep} onClick={hashRandomVideo}/> }
+      <FontAwesomeIcon className='text-accent h-9 ml-10' icon={faFire} onClick={videoPlayer} />
     </div>
   );
 }
