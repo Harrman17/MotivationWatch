ce12b36563bee558b827c2ed643dc37217418546
Harrman17 <88928675+Harrman17@users.noreply.github.com>
2024-07-23 22:24:04 +0100

    deploy

--- src/components/VideoDisplay.jsx
+++ src/components/VideoDisplay.jsx
@@ -6,7 +6,7 @@ export default function VideoDisplay({ videoLink, displayVideo, onEnded}) {
 
   return (
     <div className={`video-container flex justify-center items-center transition duration-1000 ${displayVideo ? "bg-dark" : "bg-primary"}`}>
-      {displayVideo && <video autoPlay playsInline src={videoLink} className="h-5/6" onEnded={onEnded}/>}
+      {displayVideo && <video autoPlay playsInline src={videoLink} className={`h-5/6 ${displayVideo ? "opacity-100" : ""}`} onEnded={onEnded}/>}
     </div>
   )
 }
