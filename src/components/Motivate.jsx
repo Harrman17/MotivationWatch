76907b8e6d43b70263c0c43015fc1d38c5270240
Harrman17 <88928675+Harrman17@users.noreply.github.com>
2024-09-07 17:00:21 +0100

    mobile-styling

--- src/components/Motivate.jsx
+++ src/components/Motivate.jsx
@@ -5,7 +5,7 @@ import { faFire, faForwardStep } from '@fortawesome/free-solid-svg-icons';
 
 export default function Motivate({ videoPlayer, displayVideo, hashRandomVideo }) {
   return (
-    <div className='flex fixed bottom-2 right-8 p-8 '>
+    <div className='flex fixed bottom-22 right-8 p-8 mt-20'>
       { displayVideo && <FontAwesomeIcon className={`h-9 ${displayVideo ? "text-primary" : "text-accent"}`} icon={faForwardStep} onClick={hashRandomVideo}/> }
       <FontAwesomeIcon className={`h-9 ml-10 ${displayVideo ? "text-primary" : "text-accent"}`} icon={faFire} onClick={videoPlayer} />
     </div>
