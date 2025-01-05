ac1019f5955cf536d99e51353bfa9616837e1cc9
harrman17 <harmankapoor178@gmail.com>
2025-01-05 13:49:00 +0000

    added google forms to info page for request to add/remove video

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -11,10 +11,10 @@ export default function Info() {
         <p className='info-paragraphs'>Every video you see is taken from a private Instagram collection that I've made and update regularly, these are videos that resonate with me and that I think could either motivate or be beneficial in any way to my users.</p>
         <br/>
         <h2 className='info-titles'>You've uploaded one of my videos and I want it taken down.</h2>
-        <p className='info-paragraphs'>All videos uploaded to the website are public on Instagram, however if you'd like your post taken down please fill the <Link to="/request-to-remove" className='text-accent underline'>request to remove form</Link>.</p>
+        <p className='info-paragraphs'>All videos uploaded to the website are public on Instagram, however if you'd like your post taken down please fill the <a className='text-accent underline' href="https://docs.google.com/forms/d/e/1FAIpQLSdUGXE4QAysXtlmLzHCdWyrZzgZvmmQKV3V9FwsfQjSZPhsNg/viewform">request to remove form</a>.</p>
         <br/>
         <h2 className='info-titles'>I want a motivational video uploaded!</h2>
-        <p className='info-paragraphs'>Thank you for considering a contribution, all videos submitted are reviewed and then added accordingly through the <Link className='text-accent underline' to="/request-to-add">request to add form</Link>.</p>
+        <p className='info-paragraphs'>Thank you for considering a contribution, all videos submitted are reviewed and then added accordingly through the <a className='text-accent underline' href="https://docs.google.com/forms/d/e/1FAIpQLSfMNzltsTKoWaN3rjFiRgbMSjusz_Q0fc0Y-3-Ul-0si39rLg/viewform?usp=dialog">request to add form</a>.</p>
     </div>
   )
 }
--- src/components/Motivate.jsx
+++ src/components/Motivate.jsx
@@ -6,8 +6,8 @@ import { faFire, faForwardStep } from '@fortawesome/free-solid-svg-icons';
 export default function Motivate({ videoPlayer, displayVideo, hashRandomVideo }) {
   return (
     <div className='flex fixed bottom-3 right-2 p-8 mt-20'>
-      { displayVideo && <FontAwesomeIcon className={`h-9 ${displayVideo ? "text-primary" : "text-accent"}`} icon={faForwardStep} onClick={hashRandomVideo}/> }
-      <FontAwesomeIcon className={`h-9 ml-10 ${displayVideo ? "text-primary" : "text-accent"}`} icon={faFire} onClick={videoPlayer} />
+      { displayVideo && <FontAwesomeIcon className={`h-9 cursor-pointer ${displayVideo ? "text-primary" : "text-accent"}`} icon={faForwardStep} onClick={hashRandomVideo}/> }
+      <FontAwesomeIcon className={`h-9 ml-10 cursor-pointer ${displayVideo ? "text-primary" : "text-accent"}`} icon={faFire} onClick={videoPlayer} />
     </div>
   );
 }
