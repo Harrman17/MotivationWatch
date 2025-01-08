a74c3843faff7363cff04b2f1e1eb33a2e14d416
harrman17 <harmankapoor178@gmail.com>
2025-01-08 15:01:52 +0000

    styling touchups

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom'
 export default function Header({ displayVideo, displayInfoIcon }) {
 
   return (
-    <div className={`h-20 flex justify-center items-center transition duration-1000 ${displayVideo ? "" : "bg-primary"}`}>
+    <div className={`h-20 flex justify-center items-center ${displayVideo ? "" : "bg-primary"}`}>
       <div className='flex'>
         <h1 className={`font-inter italic text-2xl text-accent transition duration-1000 ${displayVideo ? "text-opacity-25" : ""}`}>Motivation</h1>
         <h1 className={`font-inter italic text-2xl text-white transition duration-1000 ${displayVideo ? "text-opacity-25" : ""}`}>Watch</h1>
